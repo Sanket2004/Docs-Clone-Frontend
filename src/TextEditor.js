4445a612c9071efb3b4b445f62ec212d95650741
Sanket Banerjee <sanketbanerjee.2004@gmail.com>
2024-03-07 22:28:35 +0530

    quillref removed

--- src/TextEditor.js
+++ src/TextEditor.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useRef, useState } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import Quill from 'quill'
 import "quill/dist/quill.snow.css"
 import { io } from "socket.io-client";
